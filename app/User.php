42345926904d63a1e9be4f5840a9ca93825698e9
abshammeri <abshammeri@mac-shammari.local>
2015-04-06 09:40:51 +0300

    test

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
+	public function roles(){
+		
+		return $this->belongsToMany('App\Role');
+	}
+
 }
